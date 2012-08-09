7dec747ed50fd390b61aeae555e5bba01e1d8798
Jakkapong <visualizeq@gmail.com>
2012-08-09 10:56:24 +0700

    modify url_helper

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -5,7 +5,7 @@ class Auth extends CI_Controller {
   function __construct() {
     parent::__construct();
   }
-  
+
   function index() {
     $user = $this->facebook->getUser();
     if (!$user) {
@@ -14,33 +14,33 @@ class Auth extends CI_Controller {
       redirect($this->config->item('fb_canvasUrl'));
     }
   }
-  
+
   function login() {
     // generate allow application url by using facebook sdk
-    // more scope information : 
+    // more scope information :
     $params = array(
         'scope' => 'publish_stream,user_photos,email,offline_access,read_stream',
         'redirect_uri' => site_url()
     );
     $login_url = $this->facebook->getLoginUrl($params);
-    redirect($login_url, 'refresh');
+    redirect($login_url, 'script');
   }
-  
+
   function logout() {
     // destroy session and redirect
     session_destroy();
-    
+
     $params = array(
       'next' => site_url()
     );
     $logout_url = $this->facebook->getLogoutUrl($params);
-    redirect($logout_url, 'refresh');
+    redirect($logout_url, 'script');
   }
-  
+
   function success() {
     // ping facebook php sdk before redirect
     // prevent $this->facebook->getUser return 0 after redirect
     $user = $this->facebook->getUser();
-    redirect($this->config->item('fb_canvasUrl'));
+    redirect($this->config->item('fb_canvasUrl'), 'script');
   }
 }
